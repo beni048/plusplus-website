d4ceb05fda5f635fded97cb44a0e647f0e273bbf
beni048 <beni048@hotmail.com>
2025-11-25 09:30:00 +0100

    feat: add Google Tag Manager

--- app/layout.tsx
+++ app/layout.tsx
@@ -98,6 +98,19 @@ export default function RootLayout({
       <head>
         <meta charSet="utf-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
+        {/* Google Tag Manager */}
+        <Script
+          id="gtm-script"
+          strategy="afterInteractive"
+          dangerouslySetInnerHTML={{
+            __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+})(window,document,'script','dataLayer','GTM-WB7JJTSK');`
+          }}
+        />
+        {/* End Google Tag Manager */}
         <Script
           id="org-schema"
           type="application/ld+json"
@@ -106,7 +119,19 @@ export default function RootLayout({
           }}
         />
       </head>
-      <body className={`${outfit.variable} ${mulish.variable} font-sans`}>{children}</body>
+      <body className={`${outfit.variable} ${mulish.variable} font-sans`}>
+        {/* Google Tag Manager (noscript) */}
+        <noscript>
+          <iframe
+            src="https://www.googletagmanager.com/ns.html?id=GTM-WB7JJTSK"
+            height="0"
+            width="0"
+            style={{ display: 'none', visibility: 'hidden' }}
+          />
+        </noscript>
+        {/* End Google Tag Manager (noscript) */}
+        {children}
+      </body>
     </html>
   );
 }
